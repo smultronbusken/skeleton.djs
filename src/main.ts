f08f45879078034927dfd37b517d6411fe9283ef
smultronbusken <fredholmisak@outlook.com>
2023-06-09 03:30:01 +0200

    changed exports

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,32 @@
-export * from "./Skeleton";
-export * from "./options";
-export * from "./importer/ImportHandler";
-export * from "./deployer/APICommandProvider";
-export * from "./command/BaseCommand";
+export * as Skeleton from "./Skeleton";
+export * as option from "./options";
+export * as ImportHandler from "./importer/ImportHandler";
+export * as APICommandProvider from "./deployer/APICommandProvider";
+export {
+  InteractionExecutable,
+  CommandBase,
+  CommandInput,
+  ApplicationCommandNonInput,
+  ApplicationCommandNonInputOptional,
+} from "./command/BaseCommand";
 export { CommandMediator } from "./command/CommandMediator";
 export { InteractionHandler } from "./eventhandlers/InteractionHandler";
-export * from "./implementations/ContextMenuCommand/Command";
-export * from "./implementations/CustomId/Command";
+export {
+  ContextMenuCommand,
+  MessageCommand,
+  UserCommand,
+} from "./implementations/ContextMenuCommand/Command";
+export { CustomIdCommand } from "./implementations/CustomId/Command";
 export {
   CustomIdInteraction,
   isCustomInteraction,
 } from "./implementations/CustomId/InteractionHandler";
-export * from "./implementations/SlashCommand/Command";
-export * from "./implementations/SubCommand/MasterCommand";
-export * from "./implementations/SubCommand/SubCommand";
+export { SlashCommand } from "./implementations/SlashCommand/Command";
+export { MasterCommand } from "./implementations/SubCommand/MasterCommand";
+export {
+  ApplicationSubcommandNonInput,
+  ApplicationSubcommandNonInputOptional,
+  SubCommand,
+  SubcommandBase,
+  SubcommandInput,
+} from "./implementations/SubCommand/SubCommand";
