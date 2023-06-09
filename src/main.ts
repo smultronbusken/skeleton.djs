5b8e0cae8bce9f1e6040aae68c1de1c859f3943b
smultronbusken <fredholmisak@outlook.com>
2023-06-09 03:17:54 +0200

    main exports

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 export * from "./Skeleton";
+export * from "./options";
 export * from "./importer/ImportHandler";
 export * from "./deployer/APICommandProvider";
 export * from "./command/BaseCommand";
-export * from "./command/CommandMediator";
-export * from "./eventhandlers/InteractionHandler";
+export { CommandMediator } from "./command/CommandMediator";
+export { InteractionHandler } from "./eventhandlers/InteractionHandler";
 export * from "./implementations/ContextMenuCommand/Command";
 export * from "./implementations/CustomId/Command";
+export {
+  CustomIdInteraction,
+  isCustomInteraction,
+} from "./implementations/CustomId/InteractionHandler";
 export * from "./implementations/SlashCommand/Command";
 export * from "./implementations/SubCommand/MasterCommand";
 export * from "./implementations/SubCommand/SubCommand";
