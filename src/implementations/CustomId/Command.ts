52ffc71fdae7fdb7f3e48fd6dd19337a548f3bec
smultronbusken <fredholmisak@outlook.com>
2023-06-09 19:55:35 +0200

    removed CustomId interaction and typeguard

--- src/implementations/CustomId/Command.ts
+++ src/implementations/CustomId/Command.ts
@@ -1,14 +1,14 @@
+import { MessageComponentInteraction } from "discord.js";
 import { Skeleton } from "../../Skeleton";
 import { InteractionExecutable } from "../../command/BaseCommand";
 import { Importable } from "../../importer/Importer";
-import { CustomIdInteraction } from "./InteractionHandler";
 
 @Importable
 export class CustomIdCommand<T> extends InteractionExecutable<T> {
   customId: string;
   constructor(
     customId: string,
-    execute: (i: CustomIdInteraction, context: T, skeleton: Skeleton<T>) => any,
+    execute: (i: MessageComponentInteraction, context: T, skeleton: Skeleton<T>) => any,
   ) {
     super(execute);
     this.customId = customId;
--- src/implementations/CustomId/InteractionHandler.ts
+++ src/implementations/CustomId/InteractionHandler.ts
@@ -1,35 +1,28 @@
-import { BaseInteraction, Interaction } from "discord.js";
+import { BaseInteraction, Interaction, MessageComponentInteraction } from "discord.js";
 import { CommandMediator } from "../../command/CommandMediator";
 import { CustomIdCommand } from "./Command";
 import { InteractionHandler } from "../../eventhandlers/InteractionHandler";
 import { Skeleton } from "../../Skeleton";
 
-export default class CustomIdCommandInteractionHandler extends InteractionHandler<CustomIdInteraction> {
+export default class CustomIdCommandInteractionHandler extends InteractionHandler<MessageComponentInteraction> {
   constructor(public mediator: CommandMediator<CustomIdCommand<any>>) {
     super();
   }
 
-  typeGuard = (interaction: BaseInteraction): interaction is CustomIdInteraction =>
-    isCustomInteraction(interaction);
+  typeGuard = (interaction: BaseInteraction): interaction is MessageComponentInteraction =>
+    interaction.isMessageComponent();
 
   // Do not handle subcommands
-  check = (interaction: CustomIdInteraction) => {
+  check = (interaction: MessageComponentInteraction) => {
     return this.mediator.getCommands().find(cid => cid.customId === interaction.customId);
   };
 
-  execute = async (interaction: CustomIdInteraction, context: any, skeleton: Skeleton<any>) => {
+  execute = async (
+    interaction: MessageComponentInteraction,
+    context: any,
+    skeleton: Skeleton<any>,
+  ) => {
     const command = this.mediator.getCommand(interaction.customId);
     await command?.execute(interaction, context, skeleton);
   };
-}
-
-export type CustomIdInteraction = Interaction & { customId: string };
-
-export function isCustomInteraction(obj: unknown): obj is CustomIdInteraction {
-  return (
-    obj instanceof BaseInteraction &&
-    obj !== null &&
-    "customId" in obj &&
-    typeof (obj as any).customId === "string"
-  );
 }
